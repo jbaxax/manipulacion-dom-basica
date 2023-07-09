9f76ac5017cb0a1961ce114195ce2ddba38d0c2c
jbaxax <obarnettc@gmail.com>
2023-07-09 16:18:42 -0500

    Agregar .gitignore y primeros archivos de proyecto

---
+++ script2.js
@@ -0,0 +1,24 @@
+const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelectorAll('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+/* form.addEventListener('submit',sumarInputValues);
+
+function sumarInputValues(event){
+    console.log({event});
+    event.preventDefault(); 
+    const sumaInputs = input1.value+input2.value;
+    pResult.innerText = 'Resultado: '+sumaInputs
+} */
+
+form.addEventListener('click',sumarInputValues);
+
+function sumarInputValues(){
+    //console.log({event});
+    //event.preventDefault(); 
+    const sumaInputs = input1.value+input2.value;
+    pResult.innerText = 'Resultado: '+sumaInputs
+}
